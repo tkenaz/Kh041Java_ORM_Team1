c3ebdb9f5357b25d3f665966c7c842a510d57f51
Alyona Semenyakina <35637190+alenase@users.noreply.github.com>
2020-04-26 19:49:05 +0300

    Pool updated with fixes

--- src/main/java/connectiontodb/ConnectionPoll.java
+++ src/main/java/connectiontodb/ConnectionPoll.java
@@ -6,60 +6,64 @@ import java.util.List;
 
 public class ConnectionPoll {
 
-    //private DBConnection dbConnection = new DBConnection();
+    public static ConnectionPoll connectionPool;
     private static final int INITIAL_POOL_SIZE = 10;
-    private final List<Connection> connectionPool = new ArrayList<>(INITIAL_POOL_SIZE);
-    private final List<Connection> usedConnections = new ArrayList<>(INITIAL_POOL_SIZE);
+    private static final List<Connection> connectionPoolList = new ArrayList<>(INITIAL_POOL_SIZE);
+    private static final List<Connection> usedConnectionsList = new ArrayList<>(INITIAL_POOL_SIZE);
 
-    public ConnectionPoll() {
+    static {
         create();
     }
 
+    public ConnectionPoll() { }
 
-    public void create(){
+
+    public static void create(){
         for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
-            connectionPool.add(new DBConnection().connect());
+            connectionPoolList.add(new DBConnection().connect());
         }
     }
 
-    public Connection getConnection() {
-        if (connectionPool.size() < 1){
+    public static Connection getConnection() {
+        if (connectionPoolList.size() == 0){
+            System.out.println("get Connection1 ");
             return generateAdditionalConnections();
 
         }
         else {
-            Connection connection = connectionPool.remove(connectionPool.size() - 1);
-            usedConnections.add(connection);
+            Connection connection = connectionPoolList.remove(connectionPoolList.size() - 1);
+            usedConnectionsList.add(connection);
+            System.out.println("Connection sent2");
             return connection;
         }
     }
 
-    public Connection generateAdditionalConnections(){
+    public static Connection generateAdditionalConnections(){
         for (int i = 0; i < INITIAL_POOL_SIZE / 2; i++) {
-            connectionPool.add(new DBConnection().connect());
+            connectionPoolList.add(new DBConnection().connect());
         }
-        Connection connection = connectionPool.remove(connectionPool.size() - 1);
-        usedConnections.add(connection);
+        Connection connection = connectionPoolList.remove(connectionPoolList.size() - 1);
+        usedConnectionsList.add(connection);
         return connection;
     }
 
 
-    public void releaseConnection(Connection connection) {
-        connectionPool.add(connection);
-        usedConnections.remove(connection);
-        if(connectionPool.size() > 10){
-            for (int i = 10; i < connectionPool.size(); i++) {
-                connectionPool.remove(i);
+    public static void releaseConnection(Connection connection) {
+        connectionPoolList.add(connection);
+        usedConnectionsList.remove(connection);
+        if(connectionPoolList.size() > 10){
+            for (int i = 10; i < connectionPoolList.size(); i++) {
+                connectionPoolList.remove(i);
             }
         }
     }
 
     public int getSize() {
-        return connectionPool.size() + usedConnections.size();
+        return connectionPoolList.size() + usedConnectionsList.size();
     }
 
     public int getFreeConnectionPoolSize(){
-        return connectionPool.size();
+        return connectionPoolList.size();
     }
 
 
