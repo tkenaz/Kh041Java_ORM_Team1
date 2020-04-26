7dbee45c2b145fab7f765409f7024aea31cbaa36
Alyona Semenyakina <35637190+alenase@users.noreply.github.com>
2020-04-26 17:34:29 +0300

    package renamed. now all letters are from lower case

--- src/main/java/connectiontodb/ConnectionPoll.java
+++ src/main/java/connectiontodb/ConnectionPoll.java
@@ -1,9 +1,6 @@
-package ConnectionToDB;
+package connectiontodb;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/connectiontodb/DBConnection.java
+++ src/main/java/connectiontodb/DBConnection.java
@@ -1,4 +1,4 @@
-package ConnectionToDB;
+package connectiontodb;
 
 import java.io.FileInputStream;
 import java.io.IOException;
