46a4edfd216e3cad975307b9df61d9eda0e5b441
Roman <projmann@gmail.com>
2020-04-28 14:52:59 +0300

    more CRUD methods

--- src/main/java/CRUD_Services/CRUDService.java
+++ src/main/java/CRUD_Services/CRUDService.java
@@ -1,32 +1,102 @@
 package CRUD_Services;
 
+import annotations.AutoIncremented;
 import annotations.Entity;
+import annotations.FieldName;
+import annotations.Id;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class CRUDService {
     private Connection connection;
     private Class<?> entityClass;
-    private static final String SELECT_ALL = "SELECT * FROM ?";
+    private String tableName;
 
     public CRUDService(Connection connection, Class<?> entityClass) {
         this.connection = connection;
         this.entityClass = entityClass;
+        tableName = entityClass.getAnnotation(Entity.class).tableName();
     }
-    private String getTableName(){
-       return entityClass.getAnnotation(Entity.class).tableName();
+
+    void insert(Object...values){
+        String columnName;
+        Field[] fields = entityClass.getDeclaredFields();
+        StringBuilder query = new StringBuilder("INSERT INTO ");
+        StringBuilder valuesForQuery = new StringBuilder(" VALUES (");
+        query.append(tableName).append(" (");
+        for(int i = 0; i < fields.length; i++){
+            if(fields[i].getAnnotation(FieldName.class)!=null && fields[i].getAnnotation(AutoIncremented.class)==null){
+                columnName = fields[i].getAnnotation(FieldName.class).name();
+                query.append(" ").append(columnName);
+                valuesForQuery.append(" '").append(values[i].toString()).append("'");
+                if(i < fields.length - 1){
+                    query.append(",");
+                    valuesForQuery.append(",");
+                }
+            }
+        }
+        valuesForQuery.append(");");
+        query.append(")").append(valuesForQuery);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query.toString());){
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    void update(HashMap<String, String> columnsAndValuesToUpdate,String conditionalColumnName, String conditionValue){
+
+    }
+    void deleteById(int idToDelete){
+        StringBuilder query = new StringBuilder("DELETE FROM ");
+        query.append(tableName);
+        query.append(" WHERE id = ?;");
+        try(PreparedStatement delete = connection.prepareStatement(query.toString());) {
+            delete.setInt(1, idToDelete);
+            delete.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    void deleteByCondition(String conditionalColumnName, String conditionValue){
+        StringBuilder query = new StringBuilder("DELETE FROM ");
+        query.append(tableName);
+        query.append(" WHERE ").append(conditionalColumnName).append(" = ");
+        query.append("'").append(conditionValue).append("';");
+        try(PreparedStatement delete = connection.prepareStatement(query.toString());) {
+            delete.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     ResultSet selectAll(){
-        PreparedStatement select = null;
+        StringBuilder query = new StringBuilder("SELECT * FROM ");
+        query.append(tableName).append(";");
         ResultSet resultSet = null;
-        try {
-            select = connection.prepareStatement(SELECT_ALL);
-            select.setString(1, getTableName());
-            resultSet = select.getResultSet();
+        try(PreparedStatement select = connection.prepareStatement(query.append(tableName).toString());) {
+            resultSet = select.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+    ResultSet selectAllWithOrder(String columnNameToOrderBy, boolean naturalOrder){
+        StringBuilder query = new StringBuilder("SELECT * FROM ");
+        query.append(tableName).append(" ORDER BY ").append(columnNameToOrderBy);
+        if(!naturalOrder){
+            query.append(" DESC;");
+        }
+        else {
+            query.append(";");
+        }
+        ResultSet resultSet = null;
+        try(PreparedStatement select = connection.prepareStatement(query.toString());) {
+            resultSet = select.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
         }
