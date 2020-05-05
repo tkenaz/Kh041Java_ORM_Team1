7f5f8a9b58600e8b1873ce76d3e956426a7a2682
tkenaz <t.kenaz@gmail.com>
2020-05-06 01:10:22 +0300

    Initail

--- src/main/java/enums/GenerationType.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum GenerationType {
-    IDENTITY,
-    SEQUENCE;
-
-    private GenerationType() {
-    }
-}
--- src/main/java/exception/NoPrimaryKeyException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class NoPrimaryKeyException extends Exception{
-
-    public NoPrimaryKeyException(String message){
-        super(message);
-    }
-}
